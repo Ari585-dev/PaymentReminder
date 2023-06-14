14bb1841ccbd65d96bc7a373914d4f6eb03edc3b
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-06-14 16:11:33 -0500

    SQL connection created

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const app= require('./app');
-const port= 3000;
+const app = require('./app');
+const mysql = require('mysql');
+const port = 3000;
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'juegos'
+});
 
-app.listen(port, ()=> {
-    console.log("listening in 3000")
-})
+try {
+    connection.connect();
+    console.log('¡CONEXIÓN EXITOSA!');
+    app.listen(port, () => {
+        console.log("listening in "+port)
+    })
+} catch (error) {
+    console.error('Error al conectar a la base de datos: ' + error.stack);
+} finally {
 
+    connection.end();
+}
