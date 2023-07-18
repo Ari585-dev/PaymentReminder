407d06d3969620c5a735fe359f348dd583562df1
Julian <40327956+Archi657@users.noreply.github.com>
2023-07-18 18:45:06 -0500

    Create MainController.js

---
+++ controllers/MainController.js
@@ -0,0 +1,36 @@
+const MeailController = require('./MailController');
+const MessageController = require('./MessageController');
+
+let controller = {
+    
+    sendMail: function (req, res) {
+        nombre="juan"
+        MessageController.getHtmlOpenPayment(nombre, (error, html) => {
+            const mailgun= () => 
+            mg({
+                apiKey: process.env.MAILGUN_API_KEY,
+                domain: process.env.MAILGUN_DOMAIN
+            })
+            emailInfo={
+                from: `"${process.env.NAME}" <${process.env.EMAIL}>`,   
+                to: 'seragial@gmail.com',
+                subject: 'User Registered',
+                html: html
+            }
+            mailgun().messages().send(emailInfo, (error, body)=>{
+                if(error){
+                    console.log(error)
+                    res.status(500).send({
+                        message: "xd"
+                    })
+                }else{
+                    res.send({message:body})
+                    res.status(200).send()
+                }
+            })
+        });
+    },
+
+}
+
+module.exports = controller;
