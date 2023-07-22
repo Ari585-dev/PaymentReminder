8d8bee6c972e3c50cac0d13cb76711d5dcbd3986
Julian <40327956+Archi657@users.noreply.github.com>
2023-07-22 12:59:14 -0500

    + Date for each email.

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -21,19 +21,26 @@ let controller = {
         }
       },
 
-    getHtmlReminder: async function (student) {
-
-      try {
-        const html= await readFileAsync(htmlRemindURL, 'utf-8');
-        const htmlReplaced= html.replace("Nombre", student.nombres);
-        return htmlReplaced;
-
-      } catch (error) {
-        console.error(error);
-        throw error;
+      getHtmlReminder: async function (student, date) {
+        try {
+          const html = await readFileAsync(htmlRemindURL, 'utf-8');
+          let htmlReplaced = html.replace("Nombre", student.nombres);
+          console.log(date); // Just for debugging purposes to see the date value
+      
+          // Access the necessary property and extract the date value
+          const payment_opening_date = date[0].fecha_de_pago_apertura;
+          const extractedDate = payment_opening_date.toISOString().split("T")[0];
+      
+          htmlReplaced = htmlReplaced.replace("Fecha", extractedDate);
+      
+          return htmlReplaced;
+        } catch (error) {
+          console.error(error);
+          throw error;
+        }
       }
-
-    }
+      
+      
 
     
 }
