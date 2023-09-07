1550eddcfab3011ed64e1ebca27416a9f5a60d53
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-09-07 18:19:38 -0500

    Update Date.js

--- crud/Dates.js
+++ crud/Dates.js
@@ -17,6 +17,22 @@ module.exports = {
     }
   },
 
+  getExtraordinaryDate: async function (connection) {
+    const queryAsync = promisify(connection.query).bind(connection);
+    try {
+      const date = await queryAsync("SELECT extraordinary_date FROM information");
+      moment.locale("es");
+      const closing_payment_date = date[0].extraordinary_date;
+      const extractedDate = closing_payment_date.toISOString().split("T")[0];
+      const parsedDate = moment(extractedDate, "YYYY-MM-DD");
+      const formattedDate = parsedDate.format("MMMM Do YYYY");
+      return formattedDate;
+    } catch (err) {
+      throw err;
+    }
+  },
+
+
   getClosingDate: async function (connection) {
     const queryAsync = promisify(connection.query).bind(connection);
     try {
