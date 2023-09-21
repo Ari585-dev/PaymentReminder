4524b5d75a05b1490865e0d0546f8c88efeb4be2
Julian <40327956+Archi657@users.noreply.github.com>
2023-09-20 21:54:02 -0500

    Home modal added
    Have to review the design for the menu interface, and decide if picture will be added

--- mobile_frontend/App.tsx
+++ mobile_frontend/App.tsx
@@ -1,39 +1,57 @@
-import { View, Text, TextInput, Pressable, Image } from 'react-native';
+import { View, Text, TextInput, Pressable, Image, Modal } from 'react-native';
+import React, { useState } from 'react'
 import tailwind from 'twrnc';
+import Home from './Home'
 
 export default function LoginScreen() {
+
+  const [modal, setModalVisible] = useState(false)
+
+  const login = () =>{
+    setModalVisible(true)
+  }
+
+  const closeModal = () =>{
+    setModalVisible(false)
+  }
+
   return (
     <View style={tailwind`flex-1 items-center justify-center bg-slate-50`}>
       <View style={tailwind`p-8 w-full max-w-sm`}>
       <Image style={{ paddingLeft:320, width: 340, height: 340}} source={require('./img/udistritallogo.png')} />
         <Text style={tailwind`text-3xl font-bold mb-6 text-slate-900`}>Login</Text>
 
-        <TextInput
-          style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4 mb-4`}
+        <TextInput style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4 mb-4`}
           placeholderTextColor="#000"
           placeholder="Ingresa codigo de estudiante"
         />
 
-        <TextInput
-          style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4`}
+        <TextInput style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4`}
           placeholderTextColor="#000"
           placeholder="Ingresa contraseña"
         />
 
         <View style={tailwind`flex flex-row justify-between items-center my-4`}>
           <View style={tailwind`flex-row items-center`}>
-            
           </View>
         </View>
 
-        <Pressable
-          style={tailwind`h-13 bg-blue-500 rounded-md flex flex-row justify-center items-center px-6`}
+        <Pressable style={tailwind`h-13 bg-blue-500 rounded-md flex flex-row justify-center items-center px-6`}
+          onPress={()=>login()}
         >
           <View style={tailwind`flex-1 flex items-center`}>
             <Text style={tailwind`text-white text-base font-medium`}>Login</Text>
           </View>
         </Pressable>
+        
+          <Home
+            closeModal={closeModal}
+            modal={modal}
+          />
+        
       </View>
     </View>
+
+    
   );
 }
