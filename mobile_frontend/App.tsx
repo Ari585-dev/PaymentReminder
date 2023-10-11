08c147a555946ca0fad306de274916bdaeca44f0
Julian <40327956+Archi657@users.noreply.github.com>
2023-10-10 19:26:39 -0500

    Menu Interface and Login Logic
    Added new design in the menu, and logic to send the data from the login to the menu, also check fields for the login

--- mobile_frontend/App.tsx
+++ mobile_frontend/App.tsx
@@ -1,57 +1,89 @@
-import { View, Text, TextInput, Pressable, Image, Modal } from 'react-native';
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Image,
+  Modal,
+  Alert,
+} from 'react-native';
+import React, {useState} from 'react';
 import tailwind from 'twrnc';
-import Home from './Home'
+import Home from './Home';
 
 export default function LoginScreen() {
+  const [modal, setModalVisible] = useState(false);
+  const [code, setCode] = useState(0);
+  const [password, setPassword] = useState(0);
+  const [student, setStudent] = useState({});
+  //const [dates, setDates] = useState([])
+  const login = () => {
+    if ([code, password].includes('')) {
+      Alert.alert('Error', 'Ingrese todos los datos');
+      return 
+    } 
+    //if login data base
+    
+    setStudent({
+      code: code,
+      name: 'Julian Gomez',
+      career: 'Tec Sistematización de Datos',
+      paid: true,
+      fee: 123.21,
+    });
+    setModalVisible(true);
+  };
 
-  const [modal, setModalVisible] = useState(false)
-
-  const login = () =>{
-    setModalVisible(true)
-  }
-
-  const closeModal = () =>{
-    setModalVisible(false)
-  }
+  const closeModal = () => {
+    setModalVisible(false);
+  };
 
   return (
     <View style={tailwind`flex-1 items-center justify-center bg-slate-50`}>
       <View style={tailwind`p-8 w-full max-w-sm`}>
-      <Image style={{ paddingLeft:320, width: 340, height: 340}} source={require('./img/udistritallogo.png')} />
-        <Text style={tailwind`text-3xl font-bold mb-6 text-slate-900`}>Login</Text>
+        <Image
+          style={{paddingLeft: 320, width: 340, height: 340}}
+          source={require('./img/udistritallogo.png')}
+        />
+        <Text style={tailwind`text-3xl font-bold mb-6 text-slate-900`}>
+          Login
+        </Text>
 
-        <TextInput style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4 mb-4`}
+        <TextInput
+          style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4 mb-4`}
           placeholderTextColor="#000"
           placeholder="Ingresa codigo de estudiante"
+          onChangeText={setCode}
         />
 
-        <TextInput style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4`}
+        <TextInput
+          style={tailwind`w-full bg-white border border-slate-200 rounded-md h-12 px-4`}
           placeholderTextColor="#000"
           placeholder="Ingresa contraseña"
+          onChangeText={setPassword}
         />
 
         <View style={tailwind`flex flex-row justify-between items-center my-4`}>
-          <View style={tailwind`flex-row items-center`}>
-          </View>
+          <View style={tailwind`flex-row items-center`}></View>
         </View>
 
-        <Pressable style={tailwind`h-13 bg-blue-500 rounded-md flex flex-row justify-center items-center px-6`}
-          onPress={()=>login()}
-        >
+        <Pressable
+          style={tailwind`h-13 bg-blue-500 rounded-md flex flex-row justify-center items-center px-6`}
+          onPress={() => login()}>
           <View style={tailwind`flex-1 flex items-center`}>
-            <Text style={tailwind`text-white text-base font-medium`}>Login</Text>
+            <Text style={tailwind`text-white text-base font-medium`}>
+              Login
+            </Text>
           </View>
         </Pressable>
-        
-          <Home
-            closeModal={closeModal}
-            modal={modal}
-          />
-        
+
+        <Home
+          //dates={dates}
+          student={student}
+          closeModal={closeModal}
+          modal={modal}
+        />
       </View>
     </View>
-
-    
   );
 }
