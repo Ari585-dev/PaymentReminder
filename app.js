8d84eef1d8185889918863b5a01a37fe3468ec4f
Ari585-dev <129306585+Ari585-dev@users.noreply.github.com>
2023-10-23 14:33:43 -0500

    Makign corrections

--- app.js
+++ app.js
@@ -13,8 +13,7 @@ app.use(bodyParser.json())
 app.use('/api', project_routes)
 
 //jobs to check dates
-//jobs.scheduleNoPayment();
-//jobs.scheduleCheckDates();  
-//jobs.scheduleNotifyAll();
+jobs.scheduleCheckDates();  
+
 
 module.exports = app;
